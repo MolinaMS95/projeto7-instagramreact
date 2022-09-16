1b40fb981ba9f832d487b515eeadce6110239b04
MolinaMS95 <matheusmolina95@gmail.com>
2022-09-16 15:17:41 -0300

    Componentização da página

---
+++ src/Sugestoes.js
@@ -0,0 +1,70 @@
+export default function Sugestoes() {
+    return (
+        <div class="sugestoes">
+            <div class="titulo">
+                Sugestões para você
+                <div>Ver tudo</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="bad.vibes.memes.svg" />
+                    <div class="texto">
+                        <div class="nome">bad.vibes.memes</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="chibirdart.svg" />
+                    <div class="texto">
+                        <div class="nome">chibirdart</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="razoesparaacreditar.svg" />
+                    <div class="texto">
+                        <div class="nome">razoesparaacreditar</div>
+                        <div class="razao">Novo no Instagram</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="adorable_animals.svg" />
+                    <div class="texto">
+                        <div class="nome">adorable_animals</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="smallcutecats.svg" />
+                    <div class="texto">
+                        <div class="nome">smallcutecats</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+        </div>
+    )
+}
