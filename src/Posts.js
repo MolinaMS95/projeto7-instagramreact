36130d4edd1be00c69069b717da23490a310227f
MolinaMS95 <matheusmolina95@gmail.com>
2022-09-19 09:30:11 -0300

    Função de dar like - projeto finalizado

--- src/Posts.js
+++ src/Posts.js
@@ -4,16 +4,31 @@ function Post(props) {
     const path = props.name + ".svg"
     const likeImg = props.like + ".svg"
     const [saved, setSaved] = React.useState("bookmark-outline")
+    const [liked, setLiked] = React.useState("heart-outline")
+    const [filling, setFilling] = React.useState("")
+    const [likes, setLikes] = React.useState(props.likeNumber)
     
     function savePost(){
         setSaved((saved === "bookmark") ? "bookmark-outline" : "bookmark")
+    }
+
+    function likePostfromImg(){
+        setLiked("heart")
+        setFilling("liked")
+        setLikes((liked === "heart") ? likes : likes+1)
+    }
+
+    function likePost(){
+        setLiked((liked === "heart") ? "heart-outline" : "heart")
+        setFilling((filling === "liked") ? "" : "liked")
+        setLikes((liked === "heart") ? likes-1 : likes+1)
     }
 
     return (
         <div class="post">
             <div class="topo">
                 <div class="usuario">
-                    <img src={path} />
+                    <img src={path}/>
                     {props.name}
                 </div>
                 <div class="acoes">
@@ -22,13 +37,13 @@ function Post(props) {
             </div>
 
             <div class="conteudo">
-                <img src={props.img} />
+                <img src={props.img} onClick={likePostfromImg}/>
             </div>
 
             <div class="fundo">
                 <div class="acoes">
                     <div>
-                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon onClick={likePost} class={filling} name={liked}></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
@@ -40,7 +55,7 @@ function Post(props) {
                 <div class="curtidas">
                     <img src={likeImg} />
                     <div class="texto">
-                        Curtido por <strong>{props.like}</strong> e <strong>outras {props.likeNumber.toLocaleString('pt-BR')} pessoas</strong>
+                        Curtido por <strong>{props.like}</strong> e <strong>outras {likes.toLocaleString('pt-BR')} pessoas</strong>
                     </div>
                 </div>
             </div>
